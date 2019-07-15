46b66198ea0200fd8364e3e74699338a9c1251a4
Rahmenordnung <43711100+Rahmenordnung@users.noreply.github.com>
2019-07-15 13:23:58 +0100

    add information about charts, fix padding

--- js/script.js
+++ js/script.js
@@ -15,11 +15,13 @@ function makeGraphs(error, SALESData) {
         sales_month(ndx);
         scatter_price(ndx);
         show_territory_donuts(ndx);
+        show_productline(ndx);
         
          // show_Status_products(ndx);
     dc.renderAll();
     
 //selectors//
+//city selector//
 function CITY(ndx) {
         var dim = ndx.dimension(dc.pluck("CITY"));
         var group = dim.group();
@@ -28,7 +30,7 @@ function CITY(ndx) {
             .dimension(dim)
             .group(group);
     }
-
+//business name selector//
 function customer_business(ndx) {
         var dim = ndx.dimension(dc.pluck("CUSTOMERNAME"));
         var group = dim.group();
@@ -37,7 +39,7 @@ function customer_business(ndx) {
             .dimension(dim)
             .group(group);
     }    
-
+//orderdate selector//
 function amount_causes(ndx) {
         var dim = ndx.dimension(dc.pluck("ORDERDATE"));
         var group = dim.group();
@@ -48,13 +50,13 @@ function amount_causes(ndx) {
     }
 
 //graphics//
-//average monthly sale//
+//average monthly sales of all all products(bar chart)//
 function show_monthy_sales_distribution(ndx) {
     var dim = ndx.dimension(dc.pluck("MONTH_ID"));
     var group = dim.group();
 
     dc.barChart("#chart-here")
-        .width(375)
+        .width(600)
         .height(500)
         .margins({ top: 10, right: 10, bottom: 60, left: 35 })
         .dimension(dim)
@@ -63,7 +65,7 @@ function show_monthy_sales_distribution(ndx) {
         .x(d3.scale.ordinal().domain([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]).range([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]))
         .xUnits(dc.units.ordinal)
         .elasticY(true)
-        .xAxisLabel("COUNTRY")
+        .xAxisLabel("Month (numerical)")
         .renderHorizontalGridLines(true)
         .on('renderlet', function(chart) {
             chart.selectAll("g.x text")
@@ -73,7 +75,31 @@ function show_monthy_sales_distribution(ndx) {
         .addFilterHandler(function(filters, filter) { return [filter]; })
         .yAxis().ticks(20);
     }
-    
+
+//distribution of the total amont of sales per each product available(bar chart)//
+    function show_productline(ndx) {
+        var dim = ndx.dimension(dc.pluck('PRODUCTLINE'));
+        var group = dim.group();
+
+        var PRODUCTLINEColors = d3.scale.ordinal()
+                .domain(["Classic Cars", "Motorcycles", "Vintage Cars", "Planes", "Trucks and Buses", "Ships"])
+                .range(["red", "green", "blue", "yellow", "violet", "brown"]);
+        
+        dc.barChart("#products")
+            .width(600)
+            .height(500)
+            .margins({top: 10, right: 50, bottom: 60, left: 50})
+            .dimension(dim)
+            .group(group)
+            .transitionDuration(500)
+            .x(d3.scale.ordinal())
+            .xUnits(dc.units.ordinal)
+            .elasticY(true)
+            .xAxisLabel("Products")
+            .colors(PRODUCTLINEColors)
+            .yAxis().ticks(20);
+    }
+//time distribution of the sales per trimester also known as quartals(time line chart)//    
 function sales_month(ndx) {
     var QTR_ID_dim = ndx.dimension(dc.pluck("QTR_ID"));
     var total_sales_flow = QTR_ID_dim.group();
@@ -81,7 +107,7 @@ function sales_month(ndx) {
     var maxORDERDATE = QTR_ID_dim.top(1)[0].QTR_ID;
 
     dc.lineChart("#sales_quartal")
-        .width(500)
+        .width(600)
         .height(500)
         .margins({ top: 10, right: 20, bottom: 60, left: 50 })
         .dimension(QTR_ID_dim)
@@ -91,6 +117,8 @@ function sales_month(ndx) {
         .xAxisLabel("Flow-Sale")
         .yAxis().ticks(4);
     }
+
+//distribution of the prducts per sale size and status of the sale(stacked bar chart)//
 function products_per_status_dealsize(ndx) {
 
     var name_dim = ndx.dimension(dc.pluck('DEALSIZE'));
@@ -144,7 +172,7 @@ function products_per_status_dealsize(ndx) {
     });
     var stackedChart = dc.barChart("#sales_per_STATUS_and_deal_size");
     stackedChart
-        .width(500)
+        .width(600)
         .height(500)
         .margins({ top: 25, right: 20, bottom: 60, left: 90 })
         .dimension(name_dim)
@@ -157,10 +185,11 @@ function products_per_status_dealsize(ndx) {
         .x(d3.scale.ordinal())
         .xUnits(dc.units.ordinal)
         .xAxisLabel("DEALSIZE")
-        .legend(dc.legend().x(275).y(0).itemHeight(15).gap(5));
+        .legend(dc.legend().x(525).y(0).itemHeight(15).gap(5));
     stackedChart.margins().right = 100;
     
 }
+//distribution of all sales per year, stacked chart with all products stacked one above the other(stacked bar chart)//
 
 function products_per_year(ndx) {
 
@@ -223,7 +252,7 @@ function products_per_year(ndx) {
     });
     var stackedChart = dc.barChart("#sales_per_year_products");
     stackedChart
-        .width(550)
+        .width(600)
         .height(500)
         .margins({ top: 25, right: 60, bottom: 60, left: 60 })
         .dimension(name_dim)
@@ -236,10 +265,12 @@ function products_per_year(ndx) {
         .stack(SALESByPRODUCTLINETrains, "Trains")
         .x(d3.scale.ordinal())
         .xUnits(dc.units.ordinal)
-        .xAxisLabel("PRODUCTLINE")
-        .legend(dc.legend().x(275).y(0).itemHeight(15).gap(5));
+        .xAxisLabel("Year")
+        .legend(dc.legend().x(500).y(0).itemHeight(15).gap(5));
     stackedChart.margins().right = 100;
 }
+
+//sales punctual distribution per quantity of the sale and the amonut spent(scattered plot chart)//
 function scatter_price(ndx) {
     var QUANTITYORDEREDDim = ndx.dimension(function(d) {
         return d.QUANTITYORDERED;
@@ -255,13 +286,15 @@ function scatter_price(ndx) {
     var SALESGroup = SALESDim.group();
     var spend_chart = dc.scatterPlot("#price_along_dates");
     spend_chart
-        .width(750)
+        .width(600)
         .height(500)
         .x(d3.time.scale().domain([minQUANTITYORDERED, maxQUANTITYORDERED]))
         .brushOn(false)
         .symbolSize(8)
         .clipPadding(10)
         .yAxisLabel("Amount Spent")
+        .xAxisLabel("Quantity ordered")
+        .margins({ top: 25, right: 60, bottom: 60, left: 60 })
         .title(function(d) {
             return d.key[2].CUSTOMERNAME + " quatity ordered: " + d.key[2].QUANTITYORDERED + " in store " + d.key[2].CITY;
         })
@@ -275,7 +308,7 @@ function scatter_price(ndx) {
         .group(SALESGroup);
 }
 
-//distribution of the prducts per regions of distribution//
+//distribution of the prducts per world regions of delivery(donut diagramm)//
 function show_territory_donuts(ndx) {
     var dim = ndx.dimension(dc.pluck("TERRITORY"));
     var group = dim.group();
@@ -289,17 +322,17 @@ function show_territory_donuts(ndx) {
         .legend(dc.legend().x(320).y(0).itemHeight(9).gap(1));
 }
 
-//distribution of the prducts per country of distribution//
+//distribution of the prducts per country of delivery(circle diagramm)//
  function country_pie(ndx) {
         var dim = ndx.dimension(dc.pluck("COUNTRY"));
         var group = dim.group();
         dc.pieChart('#country_pie')
-            .width(375)
+            .width(425)
             .radius(200)
             .transitionDuration(1000)
             .dimension(dim)
             .group(group)
-            .legend(dc.legend().x(320).y(0).itemHeight(9).gap(1));
+            .legend(dc.legend().x(370).y(0).itemHeight(9).gap(1));
 }
 
 
