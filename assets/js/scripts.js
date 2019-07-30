e9abc49cc6808940bbcaf5ac4a52f1c6bc640b31
Rahmenordnung <43711100+Rahmenordnung@users.noreply.github.com>
2019-07-30 10:49:15 +0100

    correct intro text and one navbar element

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -24,100 +24,12 @@ $(document).ready(function() {
 });
 
 /* toggle on click hide button*/ 
-function myFunction() {
-  var x = document.getElementById("hide_botton");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
-function myFunction_sales_total() {
-  var x = document.getElementById("hide_botton_total_sales");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
-function myFunction_map() {
-  var y = document.getElementById("hide_botton_map");
-  if (y.style.display === "none") {
-    y.style.display = "block";
-  } else {
-    y.style.display = "none";
-  }
-}
-function myFunction_territories() {
-  var s = document.getElementById("hide_botton_territories");
+// 
+function showHide(id) {
+  var s = document.getElementById(id);
   if (s.style.display === "none") {
     s.style.display = "block";
   } else {
     s.style.display = "none";
   }
 }
-
-// function showHide(id) {
-//   var s = document.getElementById(id);
-//   if (s.style.display === "none") {
-//     s.style.display = "block";
-//   } else {
-//     s.style.display = "none";
-//   }
-// }
- function myFunction_country() {
-  var s = document.getElementById("hide_botton_country");
-  if (s.style.display === "none") {
-    s.style.display = "block";
-  } else {
-    s.style.display = "none";
-  }
-}
-function myFunction_month() {
-  var s = document.getElementById("hide_botton_month");
-  if (s.style.display === "none") {
-    s.style.display = "block";
-  } else {
-    s.style.display = "none";
-  }
-}
-function myFunction_year() {
-  var s = document.getElementById("hide_botton_year");
-  if (s.style.display === "none") {
-    s.style.display = "block";
-  } else {
-    s.style.display = "none";
-  }
-}
-function myFunction_time() {
-  var s = document.getElementById("hide_botton_time");
-  if (s.style.display === "none") {
-    s.style.display = "block";
-  } else {
-    s.style.display = "none";
-  }
-}
-function myFunction_product() {
-  var s = document.getElementById("hide_botton_product");
-  if (s.style.display === "none") {
-    s.style.display = "block";
-  } else {
-    s.style.display = "none";
-  }
-}
-function myFunction_deal_size() {
-  var s = document.getElementById("hide_botton_deal_size");
-  if (s.style.display === "none") {
-    s.style.display = "block";
-  } else {
-    s.style.display = "none";
-  }
-}
-function myFunction_table() {
-  var s = document.getElementById("hide_botton_table");
-  if (s.style.display === "none") {
-    s.style.display = "block";
-  } else {
-    s.style.display = "none";
-  }
-} 
